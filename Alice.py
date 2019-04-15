4a5dc5eece5ffdf5779174a3f17f10591c56a508
mod-1 <singhdivyansh98@gmail.com>
2019-04-15 22:27:20 +0800

    Handled packet corruption

--- Alice.py
+++ Alice.py
@@ -2,34 +2,39 @@ from socket import *
 import sys
 import zlib
 
-# handle the length of checksum
+
 class UDPClient:
 
     def start(self, unreliNetPort):
         servername = 'localhost'
         serverport = unreliNetPort
         clientsocket = socket(AF_INET, SOCK_DGRAM)
-        for message in sys.stdin:
-            size = len(message.encode())
-            i = 0
-            seq_no = '0'
-            while size > 0:
-                buffer = seq_no + message[i:i+50]
-                checksum, length = self.gen_checksum(buffer.encode())
-                buffer = length + checksum + buffer
-                clientsocket.sendto(buffer.encode(), (servername, serverport))
-                print('message sent: ' + buffer)
-                modifiedMessage, addr = clientsocket.recvfrom(1024)
-                print('response recieved: ' + modifiedMessage.decode())
-                if self.parse_response(modifiedMessage.decode(), seq_no):
-                    i = i + 50
-                    size = size - 50
-                    seq_no = self.compliment_seqn(seq_no)
+        seq_no = '0'
+        message = sys.stdin.read()
+        size = len(message.encode())
+        i = 0
+        while size > 0:
+            buffer = seq_no + message[i:i+50]
+            checksum, length = self.gen_checksum(buffer.encode())
+            buffer = length + checksum + buffer
+            clientsocket.sendto(buffer.encode(), (servername, serverport))
+            print('message sent: ' + buffer)
+            modifiedMessage, addr = clientsocket.recvfrom(1024)
+            print('response recieved: ' + modifiedMessage.decode())
+            if self.parse_response(modifiedMessage.decode(), seq_no):
+                i = i + 50
+                size = size - 50
+                seq_no = self.compliment_seqn(seq_no)
         clientsocket.close()
 
     def gen_checksum(self, bytes):
         checksum = zlib.crc32(bytes)
-        return str(checksum), str(len((str(checksum)).encode()))
+        length = len((str(checksum)).encode())
+        if length < 10:
+            length = '0' + str(length)
+        else:
+            length = str(length)
+        return str(checksum), length
 
     def compliment_seqn(self, sqn):
         if sqn == '0':
@@ -37,13 +42,14 @@ class UDPClient:
         return '0'
 
     def parse_response(self, message, sqn):
-        length = int(message[0:2])
-        checksum = message[2: 2 +length]
-        message_actual = message[2 + length:]
-        chksm = zlib.crc32(message_actual.encode())
-        seq_no = message_actual[-1]
-        if str(chksm) == checksum and seq_no == sqn:
-            return True
+        if message[0:2].isdigit():
+            length = int(message[0:2])
+            checksum = message[2: 2 +length]
+            message_actual = message[2 + length:]
+            chksm = zlib.crc32(message_actual.encode())
+            seq_no = message_actual[-1]
+            if str(chksm) == checksum and seq_no == sqn:
+                return True
         return False
 
 
--- Bob.py
+++ Bob.py
@@ -2,7 +2,7 @@ from socket import *
 import sys
 import zlib
 
-# handle the length of checksum
+
 class UDPServer:
 
     def start(self, rcvPort):
@@ -11,40 +11,45 @@ class UDPServer:
         seq_no = '0'
         while True:
             message, address = serverSocket.recvfrom(1024)
-            print('message received: ' + message.decode())
+            # print('message received: ' + message.decode())
             valid, res, msg_actual = self.parse_message(message.decode(), seq_no)
             if valid:
-                # print(msg_actual, end='', flush=True)
+                print(msg_actual, end='', flush=True)
                 seq_no = self.compliment_seqn(seq_no)
             serverSocket.sendto(res.encode(), address)
-            print('response sent: ' + res)
+            # print('response sent: ' + res)
         serverSocket.close()
 
     def parse_message(self, message, sqn):
-        length = int(message[0:2])
-        # print('length: ' + message[0:2])
-        checksum = message[2:2+length]
-        # print('checksum: ' + checksum)
-        message_actual = message[2+length:]
-        # print('message_actual: ' + message_actual)
-        chksm = zlib.crc32(message_actual.encode())
-        # print('chksm: ' + str(chksm))
-        seq_no = message_actual[0]
-        # print(seq_no + '   ' + sqn)
-        if str(chksm) == checksum and seq_no == sqn:
-            # print('valid message')
-            res_checksum, req_chksum_len = self.gen_checksum(('ACK'+sqn).encode())
-            res = req_chksum_len + res_checksum + 'ACK' + sqn
-            return True, res, message_actual[1:]
-        else:
-            # print('invalid message')
-            res_checksum, req_chksum_len = self.gen_checksum(('ACK' + self.compliment_seqn(sqn)).encode())
-            res = req_chksum_len + res_checksum + 'ACK' + self.compliment_seqn(sqn)
-            return False, res, message_actual[1:]
+        if message[0:2].isdigit():
+            length = int(message[0:2])
+            # print('length: ' + message[0:2])
+            checksum = message[2:2+length]
+            # print('checksum: ' + checksum)
+            message_actual = message[2+length:]
+            # print('message_actual: ' + message_actual)
+            chksm = zlib.crc32(message_actual.encode())
+            # print('chksm: ' + str(chksm))
+            seq_no = message_actual[0]
+            # print(seq_no + '   ' + sqn)
+            if str(chksm) == checksum and seq_no == sqn:
+                # print('valid message')
+                res_checksum, req_chksum_len = self.gen_checksum(('ACK'+sqn).encode())
+                res = req_chksum_len + res_checksum + 'ACK' + sqn
+                return True, res, message_actual[1:]
+        # print('invalid message')
+        res_checksum, req_chksum_len = self.gen_checksum(('ACK' + self.compliment_seqn(sqn)).encode())
+        res = req_chksum_len + res_checksum + 'ACK' + self.compliment_seqn(sqn)
+        return False, res, ''
 
     def gen_checksum(self, bytes):
         checksum = zlib.crc32(bytes)
-        return str(checksum), str(len((str(checksum)).encode()))
+        length = len((str(checksum)).encode())
+        if length < 10:
+            length = '0' + str(length)
+        else:
+            length = str(length)
+        return str(checksum), length
 
     def compliment_seqn(self, sqn):
         if sqn == '0':
